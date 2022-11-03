751f3a6f44b53f84c7381ed19fc13e3e821c8dc8
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2022-11-03 15:48:31 +0000

    Add minus() function
    minus() decrements the number of hit points displayed

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let hitPointsEl = document.getElementById("hit-points-el");
 
 let hitPoints = 0;
 
+function minus() {
+    hitPoints = hitPoints - 1;
+    hitPointsEl.innerText = hitPoints;
+}
+
 function add() {
     hitPoints = hitPoints + 1;
     hitPointsEl.innerText = hitPoints;
